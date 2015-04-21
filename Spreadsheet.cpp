86cbfe8994fdae725bdc574ec632b8c249474d7f
ImpDesign <impdesign.bm@gmail.com>
2015-04-21 17:00:16 -0600

    changed normalize function

--- Spreadsheet.cpp
+++ Spreadsheet.cpp
@@ -227,8 +227,13 @@ void Spreadsheet::Visit(string start, string name, vector<string>& visited, vect
 
 string Spreadsheet::normalize(string content)
 {
+  string change;
   locale loc;
-  return toupper(content, loc);
+  for(i = 0; i < content.length(); i++)
+  {
+	  change += toupper(content[i],loc);
+  }
+  return change;
 }
 
 vector<string> Spreadsheet::getVariables(string content)
